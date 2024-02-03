7eeb4a044a3a69bbbb54d31925856150c0c4ab08
ravi1 <ravi1@RAVI123>
2024-02-03 16:07:58 +0530

    first commit

---
+++ src/views/Home.jsx
@@ -0,0 +1,16 @@
+import Banner from "../components/Banner";
+import CreateProposal from "../components/CreateProposal";
+import Proposals from "../components/Proposals";
+
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+      <CreateProposal />
+      <Proposals />
+    </>
+  )
+}
+
+export default Home
